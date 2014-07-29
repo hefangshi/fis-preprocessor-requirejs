d23b7e2fc7785fc3c23542a8e984b3129978da5d
hefangshi <hefangshi@gmail.com>
2014-07-29 13:16:54 +0800

    暴露外部接口，优化ID规范化规则

--- index.js
+++ index.js
@@ -1,51 +1,47 @@
 var transform = require('./lib/transform.js');
+var idNormalizer = require('./lib/idNormalizer.js');
 
-module.exports = function (content, file, opt) {
-
-    function idNormalizer(file, path){
-        var namespace = fis.config.get('namespace'),
-            connector = fis.config.get('namespaceConnector', ':'),
-            id;
-        if (path.indexOf('.') !== -1){
-            return fis.file.wrap(file.dirname + '/' + path + '.js').getId();
-        }else if (['require','module','exports'].indexOf(path) !== -1){
-            return path;
-        }else if (/^(http|https):\/\//i.test(path)){
-            //外部链接直接使用原路径
-            return path;
-        }else if (namespace && path.split(connector).length === 1){
-            //指定依赖不包含namespace时添加namespace
-            return namespace + connector + path;
-        }else{
-            return path;
-        }
-    }
-
-    function extHtml(file, content, opt){
-        var reg = /(<script(?:(?=\s)[\s\S]*?["'\s\w\/\-]>|>))([\s\S]*?)(?=<\/script\s*>|$)|<!--(?!\[)([\s\S]*?)(-->|$)/ig;
-        var replace = function(m, $1, $2, $3, $4, $5, $6, $7, $8){
-            if($1){//<script>
-                if (/(\ssrc\s*=\s*)('[^']+'|"[^"]+"|[^\s\/>]+)/ig.test($1) === false){
-                    if(!/\s+type\s*=/i.test($1) || /\s+type\s*=\s*(['"]?)text\/javascript\1/i.test($1)) {
-                        //处理内嵌脚本
-                        m = $1 + extJs(file ,$2, opt);
-                    }
+function extHtml(file, content, opt){
+    var reg = /(<script(?:(?=\s)[\s\S]*?["'\s\w\/\-]>|>))([\s\S]*?)(?=<\/script\s*>|$)|<!--(?!\[)([\s\S]*?)(-->|$)/ig;
+    var replace = function(m, $1, $2, $3, $4, $5, $6, $7, $8){
+        if($1){//<script>
+            if (/(\ssrc\s*=\s*)('[^']+'|"[^"]+"|[^\s\/>]+)/ig.test($1) === false){
+                if(!/\s+type\s*=/i.test($1) || /\s+type\s*=\s*(['"]?)text\/javascript\1/i.test($1)) {
+                    //处理内嵌脚本
+                    m = $1 + extJs(file ,$2, opt);
                 }
             }
-            return m;
-        };
-        return content.replace(reg, replace);
-    }
+        }
+        return m;
+    };
+    return content.replace(reg, replace);
+}
 
-    function extJs(file, content,  opt){
-        return transform(file, content, idNormalizer);
+function extJs(file, content,  opt){
+    var asyncList = [];
+    content = transform(file, content, idNormalizer, asyncList);
+    if (asyncList.length){
+        if (file.extras.requirejs && file.extras.requirejs.syncLoad){
+            asyncList.forEach(function(id){
+                file.addRequire(id);
+            });
+        }else{
+            file.extras.async = file.extras.async || [];
+            file.extras.async = file.extras.async.concat(asyncList);
+        }
     }
+    return content;
+}
 
+module.exports = function (content, file, opt) {
     if (file.isHtmlLike) {
         content = extHtml(file, content, opt);
     }else if (file.isJsLike){
         content = extJs(file, content, opt);
     }
-
     return content;
 };
+
+module.exports.extJs = extJs;
+module.exports.transform = transform;
+module.exports.idNormalizer = idNormalizer;
